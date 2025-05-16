30a9c5ef75f92fe736d00e14cd5fe125b880b017
Dharmeet Singh <mic15614@adobe.com>
2025-05-16 14:07:01 +0530

    Updated package name.

--- Builder/BuilderDemo.java
+++ Builder/BuilderDemo.java
@@ -1,4 +1,4 @@
-package Builder;
+package DesignPatterns.Builder;
 
 public class BuilderDemo {
     public static void main(String[] args) {
--- Builder/HttpRequest.java
+++ Builder/HttpRequest.java
@@ -1,4 +1,4 @@
-package Builder;
+package DesignPatterns.Builder;
 
 import java.util.HashMap;
 import java.util.Map;
